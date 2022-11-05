4dd5192d981c05f47a20e0e88ccb51a5dac2ac65
DanioFiore <daniofiore@hotmail.com>
2022-11-05 18:56:50 +0100

    replaced the past navbar with a new tailwind bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
   return (
     <React.Fragment>
       <h1 className="text-7xl">Danio Fiore</h1>
-      <br />
+      <h5>Jr. Web Developer</h5>
     </React.Fragment>
   );
 };
