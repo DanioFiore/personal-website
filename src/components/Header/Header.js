4bd74d4acfc652f50484a5f360b0bcb243aa17e7
DanioFiore <daniofiore@hotmail.com>
2022-11-05 17:02:06 +0100

    added logo, favicon and h1

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <h1 className="text-7xl">Danio Fiore</h1>
+      <br />
+    </React.Fragment>
+  );
+};
+
+export default Header;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,21 @@
 import React from "react";
+import mainLogo from "../img/main-logo-notitle.png";
 
 const Navbar = (props) => {
     return(
         <React.Fragment>
-            <nav className="flex justify-between p-5 bg-primary-light rounded-b-md sticky top-0">
-                <ul className="text-secondary">
+            <nav className="flex justify-between bg-primary-light rounded-b-md sticky top-0">
+                <ul className="text-secondary flex items-center">
                     <a className="m-2" href="#">Home</a>
                     <a className="m-2" href="#">Projects</a>
                     <a className="m-2" href="#">MyStory</a>
                     <a className="m-2" href="#">Blog</a>
 
                 </ul>
-                <p>LOGO</p>
-                <ul className="text-secondary">
+
+                <img src={mainLogo} alt="logo" className="h-16"/>
+
+                <ul className="text-secondary flex items-center">
                     <a className="m-2" href="#">Linkedin</a>
                     <a className="m-2" href="#">Github</a>
                     <a className="m-2" href="#">Instagram</a>
